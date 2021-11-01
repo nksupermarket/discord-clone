39e26039dcf40729eec30ae51c8fb03672927d44
alex <alexlhliang@gmail.com>
2021-10-31 18:22:41 -0700

    transitioning to react-mentions, working on aligning text in textarea

--- src/components/Chat/ChatBar.js
+++ src/components/Chat/ChatBar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 
+import { MentionsInput, Mention } from 'react-mentions';
 import IconBtn from '../IconBtn';
 import MentionWrapper from './MentionWrapper';
 import MentionsPopup from './MentionsPopup';
@@ -18,80 +19,6 @@ const ChatBar = ({ roomName, replyTo, setReplyTo, userList, submit }) => {
 
   let mentionStart = useRef();
 
-  function parseHTMLForMentions(html) {
-    return html
-      .split(' ')
-      .filter((str) => str.includes('data-uid'))
-      .map((dataAtt) => {
-        const [, uid] = dataAtt.split('=');
-        return uid;
-      });
-  }
-
-  function splitHTMLBySpan(el) {
-    const clone = el.cloneNode(true);
-
-    clone.querySelectorAll('span').forEach((span) => {
-      clone.innerHTML = clone.innerHTML.replace(
-        span.outerHTML,
-        '⠀' + span.outerHTML + '⠀'
-      ); // not empty space, special character U+2800
-      console.log(clone.innerHTML);
-    });
-
-    const str = clone.innerHTML.split('⠀'); // not empty space, special character U+2800
-    console.log(str);
-  }
-
-  function getCaretIndex(el) {
-    let position = 0;
-
-    const isSupported = typeof window.getSelection !== 'undefined';
-    if (!isSupported) return;
-
-    const selection = window.getSelection();
-    if (selection.rangeCount === 0) return;
-
-    const range = selection.getRangeAt(0);
-    const preCaretRange = range.cloneRange();
-    preCaretRange.selectNode(el);
-    preCaretRange.setEnd(range.endContainer, range.endOffset);
-    position = preCaretRange.toString().length;
-
-    return position;
-  }
-
-  function getHTMLCaretIndex(el) {
-    let textPosition = getCaretIndex(el),
-      htmlContent = el.innerHTML.replace('&nbsp;', ' '),
-      textIndex = 0,
-      htmlIndex = 0,
-      insideHTML = false,
-      htmlBegin = ['<span', '</span'],
-      htmlEnd = '>';
-
-    while (textIndex < textPosition) {
-      const i = htmlIndex; //get around eslint no-loop-func error
-      if (htmlBegin.some((tag) => htmlContent.indexOf(tag, i) === i))
-        //check if character is html, if true iterate with htmlIndex until tag ends
-        insideHTML = true;
-
-      while (insideHTML) {
-        if (htmlContent.indexOf(htmlEnd, htmlIndex) === htmlIndex) {
-          insideHTML = false;
-          break; //need to exit loop otherwise we increment htmlIndex twice
-        }
-        htmlIndex++;
-      }
-
-      htmlIndex++;
-      if (!htmlBegin.some((tag) => htmlContent.indexOf(tag, i) === i))
-        // check if character is html after incrementing eg. </span><span>
-        textIndex++;
-    }
-    return htmlIndex;
-  }
-
   let style = replyTo
     ? { borderTopLeftRadius: 0, borderTopRightRadius: 0 }
     : { borderTopLeftRadius: '8px', borderTopRightRadius: '8px' };
@@ -102,9 +29,27 @@ const ChatBar = ({ roomName, replyTo, setReplyTo, userList, submit }) => {
         <IconBtn svg={addCircleSvg} alt="upload a file" />
       </div>
       <div className="input-wrapper">
-        {!msg && <div className="default-text">message {roomName}</div>}
+        {/*!msg && <div className="default-text">message {roomName}</div>*/}
         {isMentionPopup && <MentionsPopup userList={userList} query={query} />}
-        <span
+        <MentionsInput
+          className="textarea"
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+        >
+          <Mention trigger="@" data={userList} />
+        </MentionsInput>
+      </div>
+      <div className="btn-ctn">
+        <IconBtn icon="flaticon-gif" isRectangle={true} />
+        <IconBtn icon="flaticon-happy" />
+      </div>
+    </div>
+  );
+};
+
+export default ChatBar;
+
+/*<span
           ref={inputRef}
           className="textarea"
           contentEditable
@@ -170,73 +115,8 @@ const ChatBar = ({ roomName, replyTo, setReplyTo, userList, submit }) => {
                 return;
             }
           }}
-        >
-          <MentionWrapper
-            displayName={userList[0].displayName}
-            uid={userList[0].uid}
-          />
-          <span>&nbsp;</span>
-          {/*
-              return (
-                <>
-                  <MentionWrapper
-                    key={i}
-                    displayName={obj.displayName}
-                    uid={obj.uid}
-                    id={obj.id}
-                  />
-                  <span>&nbsp;</span>
-                </>
-              );
-            */}
-        </span>
-        {/*<input
-          ref={inputRef}
-          type="text"
-          name="chat"
-          value={msg}
-          placeholder={`message #${roomName}`}
-          onChange={(e) => setMsg(e.target.value)}
-          onKeyDown={(e) => {
-            switch (e.key) {
-              case 'Enter': {
-                //submit message
-                if (!msg) return;
-                const replyToMsgID = replyTo ? replyTo.msgId : null;
-                const mentionObj = mentions ? mentions : null;
-                setReplyTo();
-                submit(msg, replyToMsgID, mentionObj);
-                setMsg('');
-                if (mentions) setMentions();
-                break;
-              }
-              case 'Backspace' || 'Delete': {
-                if (!mentions) return;
-                const mentionsThatExistInMsg = mentions
-                  .map((mention) => {
-                    if (msg.includes(`@${mention.displayName}`)) return mention;
-                    return null;
-                  })
-                  .filter((val) => val);
-
-                setMentions(mentionsThatExistInMsg);
-                break;
-              }
-              default:
-                return;
-            }
-          }}
-        />*/}
-      </div>
-      <div className="btn-ctn">
-        <IconBtn icon="flaticon-gif" isRectangle={true} />
-        <IconBtn icon="flaticon-happy" />
-      </div>
-    </div>
-  );
-};
-
-export default ChatBar;
+        ></span>
+        */
 
 function getCaretCoords() {
   let x = 0,
@@ -257,4 +137,78 @@ function getCaretCoords() {
   }
 
   return { x, y };
+}
+
+function parseHTMLForMentions(html) {
+  return html
+    .split(' ')
+    .filter((str) => str.includes('data-uid'))
+    .map((dataAtt) => {
+      const [, uid] = dataAtt.split('=');
+      return uid;
+    });
+}
+
+function splitHTMLBySpan(el) {
+  const clone = el.cloneNode(true);
+
+  clone.querySelectorAll('span').forEach((span) => {
+    clone.innerHTML = clone.innerHTML.replace(
+      span.outerHTML,
+      '⠀' + span.outerHTML + '⠀'
+    ); // not empty space, special character U+2800
+    console.log(clone.innerHTML);
+  });
+
+  const str = clone.innerHTML.split('⠀'); // not empty space, special character U+2800
+  console.log(str);
+}
+
+function getCaretIndex(el) {
+  let position = 0;
+
+  const isSupported = typeof window.getSelection !== 'undefined';
+  if (!isSupported) return;
+
+  const selection = window.getSelection();
+  if (selection.rangeCount === 0) return;
+
+  const range = selection.getRangeAt(0);
+  const preCaretRange = range.cloneRange();
+  preCaretRange.selectNode(el);
+  preCaretRange.setEnd(range.endContainer, range.endOffset);
+  position = preCaretRange.toString().length;
+
+  return position;
+}
+
+function getHTMLCaretIndex(el) {
+  let textPosition = getCaretIndex(el),
+    htmlContent = el.innerHTML.replace('&nbsp;', ' '),
+    textIndex = 0,
+    htmlIndex = 0,
+    insideHTML = false,
+    htmlBegin = ['<span', '</span'],
+    htmlEnd = '>';
+
+  while (textIndex < textPosition) {
+    const i = htmlIndex; //get around eslint no-loop-func error
+    if (htmlBegin.some((tag) => htmlContent.indexOf(tag, i) === i))
+      //check if character is html, if true iterate with htmlIndex until tag ends
+      insideHTML = true;
+
+    while (insideHTML) {
+      if (htmlContent.indexOf(htmlEnd, htmlIndex) === htmlIndex) {
+        insideHTML = false;
+        break; //need to exit loop otherwise we increment htmlIndex twice
+      }
+      htmlIndex++;
+    }
+
+    htmlIndex++;
+    if (!htmlBegin.some((tag) => htmlContent.indexOf(tag, i) === i))
+      // check if character is html after incrementing eg. </span><span>
+      textIndex++;
+  }
+  return htmlIndex;
 }
